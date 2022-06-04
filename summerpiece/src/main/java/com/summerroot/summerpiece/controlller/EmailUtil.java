57491563cc3ad981438b2169b7e026b23fd86fa2
oos43 <ys0430p@naver.com>
2022-06-04 21:27:07 +0900

    회원 이메일 코드 전송 기능

---
+++ summerpiece/src/main/java/com/summerroot/summerpiece/controlller/EmailUtil.java
@@ -0,0 +1,37 @@
+package com.summerroot.summerpiece.controlller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class EmailUtil {
+
+    @Autowired
+    private JavaMailSender sender;
+
+    public Map<String, Object> sendEmail(String email, String subject, String body) {
+        Map<String, Object> result = new HashMap<>();
+        MimeMessage message = sender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message);
+
+        try {
+            helper.setTo(email);
+            helper.setSubject(subject);
+            helper.setText(body);
+            result.put("resultCode", 200);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            result.put("resultCode", 500);
+        }
+
+        sender.send(message);
+        return result;
+    }
+}
